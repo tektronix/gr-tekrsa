12a7878e7e451aa58111163c950fa508ea14564b
Cole French <colew.french@gmail.com>
2019-04-20 14:30:56 -0700

    Add swig3.0 to installed package list

--- build_env_new_rsa_api.sh
+++ build_env_new_rsa_api.sh
@@ -3,7 +3,7 @@
 #this script installs gnuradio companion and its dependencies, as well as the new RSA API
 
 #install gnuradio and its dependencies:
-apt-get install -y build-essential gnuradio libusb-1.0-0 libusb-1.0-0-dev libusb-dev libudev1 libudev-dev libboost-all-dev cmake gnuradio-dev doxygen
+apt-get install -y build-essential gnuradio libusb-1.0-0 libusb-1.0-0-dev libusb-dev libudev1 libudev-dev libboost-all-dev cmake gnuradio-dev doxygen swig3.0
 
 #setup the RSA API
 mkdir RSA_API-0.13.211
