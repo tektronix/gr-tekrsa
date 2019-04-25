5f7bb8d20e60f520db4cf5e88331eb1d0e2a54c9
David Huddleson <david.huddleson@tektronix.com>
2019-04-25 09:43:09 -0700

    setup_build_env.sh: Fix stale permissions in /dev.

--- setup_build_env.sh
+++ setup_build_env.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
-#this script installs gnuradio companion and its dependencies, as well as the new RSA API
+# This script installs gnuradio-companion and its dependencies, as well as the new RSA API
 
-#install gnuradio and its dependencies:
-apt-get install -y build-essential gnuradio libusb-1.0-0 libusb-1.0-0-dev libusb-dev libudev1 libudev-dev libboost-all-dev cmake gnuradio-dev doxygen swig3.0
+script_dir="$(CDPATH= cd -- $(dirname -- $0) && pwd)"
+script_user="$(pstree -lu -s $$ | sed -n 's/[^(]*(\([^)]*\).*/\1/p')"
 
-#setup the RSA API
-mkdir RSA_API-0.13.211
-tar xvzf "RSA_API-0.13.211.tar.gz" -C ./RSA_API-0.13.211
+# Install gnuradio-companion and its dependencies
+apt-get update && \
+apt-get install -y \
+    build-essential \
+    cmake \
+    doxygen \
+    gnuradio \
+    gnuradio-dev \
+    libboost-all-dev \
+    libudev-dev \
+    libudev1 \
+    libusb-1.0-0 \
+    libusb-1.0-0-dev \
+    libusb-dev \
+    swig3.0
+
+# Setup the RSA API
+mkdir -p "${script_dir}/RSA_API-0.13.211"
+tar xvzf "${script_dir}/RSA_API-0.13.211.tar.gz" -C "${script_dir}/RSA_API-0.13.211"
 
 mkdir -p /opt/include
-cp RSA_API-0.13.211/RSA_API.h /opt/include
+cp "${script_dir}/RSA_API-0.13.211/RSA_API.h" /opt/include
 chmod -R 777 /opt/include
 
 mkdir -p /opt/lib
-cp RSA_API-0.13.211/libRSA_API.so /opt/lib
-chmod -R 777 /opt/include
+cp "${script_dir}/RSA_API-0.13.211/libRSA_API.so" /opt/lib
+chmod -R 777 /opt/lib
 
-cp RSA_API-0.13.211/cyusb.conf /etc/
+cp "${script_dir}/RSA_API-0.13.211/cyusb.conf" /etc
 
 # Setup the plugdev group
-groupadd plugdev
-usermod -a -G plugdev $(whoami)
-touch /etc/udev/rules.d/Tektronix.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0699", MODE="0666", GROUP="plugdev", ATTR{power/autosuspend}="-1", ATTR{power/control}="on", ATTR{bMaxPower}="500mA"' >> /etc/udev/rules.d/Tektronix.rules
+if ! getent group plugdev > /dev/null 2>&1
+then
+    groupadd plugdev
+fi
+usermod -a -G plugdev "${script_user}"
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0699", MODE="0666", GROUP="plugdev", ATTR{power/autosuspend}="-1", ATTR{power/control}="on", ATTR{bMaxPower}="500mA"' > /etc/udev/rules.d/Tektronix.rules
 chmod a+r /etc/udev/rules.d/Tektronix.rules
 
+# Re-create /dev entries with correct group and mode
+udevadm trigger --subsystem-match="usb"
+
 # Build the RSA Blocks
-mkdir oot/gr-TekRSA/build
-cd oot/gr-TekRSA/build
+OLDPWD="$(pwd)"
+mkdir "${script_dir}/oot/gr-TekRSA/build"
+cd "${script_dir}/oot/gr-TekRSA/build"
 cmake ..
 make
 make install
-cd ../../..
-
+cd "${OLDPWD}"
 
